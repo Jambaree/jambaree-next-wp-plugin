fed1f51d3ea0ddddc427129f4dd1d46b6e9a9169
Caleb Barnes <caleb.d.barnes@gmail.com>
2023-12-07 22:41:35 -0800

    Update jambaree.php

--- jambaree.php
+++ jambaree.php
@@ -3,7 +3,7 @@
 * Plugin Name: Jambaree Headless Wordpress - Next.js Utilities
 * Plugin URI: https://github.com/Jambaree/jambaree-next-wp-plugin
 * Description: Everything you need for a headless Wordpress sites in one place
-* Version: 2.5.1
+* Version: 2.6.0
 * Author: Jambaree
 * Author URI: https://jambaree.com/
 */
@@ -35,3 +35,16 @@ include_once('acf/populate-post-type-choices.php');
 include_once('wp/customizer-iframe-preview.php');
 
 require_once(plugin_dir_path(__FILE__) . 'includes/console_log.php');
+
+add_action('init', 'handle_preflight');
+function handle_preflight() {
+    // This is for CORS when using the WP REST API, especially when submitting Gravity Forms
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
+    header("Access-Control-Allow-Headers: Origin, Content-Type, Accept");
+
+    if('OPTIONS' == $_SERVER['REQUEST_METHOD']) {
+        status_header(200);
+        exit();
+    }
+}
